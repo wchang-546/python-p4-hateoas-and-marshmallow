947b1a0782aeff89b75f8d63577a5b889f2aad24
Will Chang <william.chaang@gmail.com>
2023-08-17 11:57:04 -0600

    Practiced Marshmallow. Strange syntax.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, make_response
-from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_marshmallow import Marshmallow
 
 from models import db, Newsletter
 
@@ -14,8 +14,29 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
-
 api = Api(app)
+
+#Added
+ma = Marshmallow(app)
+
+class NewsletterSchema(ma.SQLAclhemySchema):
+    class Meta: 
+        model = Newsletter
+        load_instance = True
+    
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+                "collection": ma.URLFor("newsletters"),
+        }
+    )
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
 
 class Index(Resource):
 
@@ -37,15 +58,24 @@ api.add_resource(Index, '/')
 class Newsletters(Resource):
 
     def get(self):
-        
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        #New Marshmallow code 
+        newsletters = Newsletter.query.all()
 
         response = make_response(
-            response_dict_list,
-            200,
+            newsletters_schema.dump(newsletters),
+            200
         )
-
         return response
+    
+        #Old code
+        # response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+
+        # response = make_response(
+        #     response_dict_list,
+        #     200,
+        # )
+
+        # return response
 
     def post(self):
         
@@ -57,13 +87,16 @@ class Newsletters(Resource):
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
+        # response_dict = new_record.to_dict()
 
+        # response = make_response(
+        #     response_dict,
+        #     201,
+        # )
         response = make_response(
-            response_dict,
-            201,
+            newsletter_schema.dump(new_record),
+            201
         )
-
         return response
 
 api.add_resource(Newsletters, '/newsletters')
@@ -71,15 +104,22 @@ api.add_resource(Newsletters, '/newsletters')
 class NewsletterByID(Resource):
 
     def get(self, id):
-
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        response_ma = Newsletter.query.filter_by(id=id).first()
 
         response = make_response(
-            response_dict,
-            200,
+            newsletter_schema.dump(response_ma),
+            200
         )
-
         return response
+
+        # response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        # response = make_response(
+        #     response_dict,
+        #     200,
+        # )
+
+        # return response
 
     def patch(self, id):
 
@@ -90,10 +130,8 @@ class NewsletterByID(Resource):
         db.session.add(record)
         db.session.commit()
 
-        response_dict = record.to_dict()
-
         response = make_response(
-            response_dict,
+            newsletter_schema.dump(record),
             200
         )
 
